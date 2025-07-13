ae90636805e70e1276ae69fa6daa5f43c770b45f
kingsleyiwuchukwu <kingsleyiwuchukwu0@gmail.com>
2025-07-13 13:19:01 +0100

    Namespace & Declaration merging

---
+++ 0x04-TypeScript/task_4/js/subjects/Java.ts
@@ -0,0 +1,21 @@
+/// <reference path="./Subject.ts" />
+
+namespace Subjects {
+  // Declaration merging to add experienceTeachingJava
+  export interface Teacher {
+    experienceTeachingJava?: number;
+  }
+
+  export class Java extends Subject {
+    getRequirements(): string {
+      return 'Here is the list of requirements for Java';
+    }
+
+    getAvailableTeacher(): string {
+      if (this.teacher && this.teacher.experienceTeachingJava > 0) {
+        return `Available Teacher: ${this.teacher.firstName}`;
+      }
+      return 'No available teacher';
+    }
+  }
+}
